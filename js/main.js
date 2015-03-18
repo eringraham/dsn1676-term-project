1d0dfb25abd004f8f86fb6192f80fe36ca42d9d8
eringraham <eringraham@Erins-MacBook-Pro.local>
2015-03-17 23:46:17 -0400

    started js tabs

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
-var $menuBtn = $('.menu-btn')
-var $fullList = $('.full-list')
+var $menuBtn = $('.menu-btn');
+var $fullList = $('.full-list');
 
-
-$menuBtn.on('click', function (e) {
-    console.log('clicked menu-btn')
+$menuBtn.on('click', function () {
     $fullList.toggleClass('js-menu-open');
-
-        
+});
+    
+    
+var $tabs = $('.tabs');
+var $panel = $('.panel');
+    
+$tabs.on('click', 'a', function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href');
+    
+    $panel.filter(':not([hidden])').attr('hidden', true);
+    $(id).removeAttr('hidden');
+    
+    $tabs.find('.js-current').removeClass('js-current');
+    $(this).addClass('js-current');
+    
 });
 
